e6cd760c9e301acd4d5fd9a6731635efa194a774
LeonMilosevic <leonleon787@yahoo.com>
2020-03-10 22:50:49 +0200

    update

--- src/components/main/Inspiration.js
+++ src/components/main/Inspiration.js
@@ -6,6 +6,15 @@ import { Link } from "react-router-dom";
 import CloseIcon from "@material-ui/icons/Close";
 
 const Inspiration = () => {
+  // array containing images to loop over and asign to img element
+  const imgArr = [
+    "https://res.cloudinary.com/clothify/image/upload/v1578599563/Asset_24_fgewym.png",
+    "https://res.cloudinary.com/clothify/image/upload/v1578599563/Asset_23_kb8qfh.png",
+    "https://res.cloudinary.com/clothify/image/upload/v1575472209/a2b_pjnchk.jpg",
+    "https://res.cloudinary.com/clothify/image/upload/v1574116778/wish_fnfavj.jpg",
+    "https://res.cloudinary.com/clothify/image/upload/v1574116544/cart_ninize.jpg"
+  ];
+
   // declare ref for better dom manipulation with gsap
   let close = useRef(null);
   // on mount start animations
@@ -29,6 +38,63 @@ const Inspiration = () => {
       >
         <CloseIcon style={{ fontSize: "56px" }} />
       </Link>
+      <div className="inspiration_container">
+        <div className="div_image">
+          <img
+            style={{
+              position: "absolute",
+              width: "150px",
+              height: "150px"
+            }}
+            src="https://res.cloudinary.com/clothify/image/upload/v1578599563/Asset_24_fgewym.png"
+            alt=""
+          />
+        </div>
+        <div className="div_image">
+          <img
+            style={{
+              position: "absolute",
+              width: "150px",
+              height: "150px"
+            }}
+            src="https://res.cloudinary.com/clothify/image/upload/v1578599563/Asset_23_kb8qfh.png"
+            alt=""
+          />
+        </div>
+        <div className="div_image">
+          <img
+            style={{
+              position: "absolute",
+              width: "150px",
+              height: "150px"
+            }}
+            src="https://res.cloudinary.com/clothify/image/upload/v1575472209/a2b_pjnchk.jpg"
+            alt=""
+          />
+        </div>
+        <div className="div_image">
+          <img
+            style={{
+              position: "absolute",
+              width: "150px",
+              height: "150px"
+            }}
+            src="https://res.cloudinary.com/clothify/image/upload/v1574116778/wish_fnfavj.jpg"
+            alt=""
+          />
+        </div>
+        <div className="div_image">
+          <img
+            style={{
+              position: "absolute",
+              width: "150px",
+              height: "150px"
+            }}
+            src="https://res.cloudinary.com/clothify/image/upload/v1574116544/cart_ninize.jpg"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
