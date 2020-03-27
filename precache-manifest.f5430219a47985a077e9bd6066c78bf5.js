d43782cd25aac3d9d9dc4d28575c0fe4bcb6c505
LeonMilosevic <leonleon787@yahoo.com>
2020-03-27 10:35:11 +0200

    Updates

---
+++ precache-manifest.f5430219a47985a077e9bd6066c78bf5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d61cc01306155e52c596547cbdc5f53",
+    "url": "/domas/index.html"
+  },
+  {
+    "revision": "c0c6a13b13a7bbd10cff",
+    "url": "/domas/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "0e7af5b5e78223605fd8",
+    "url": "/domas/static/css/main.3b9e8f60.chunk.css"
+  },
+  {
+    "revision": "c0c6a13b13a7bbd10cff",
+    "url": "/domas/static/js/2.197989c6.chunk.js"
+  },
+  {
+    "revision": "a691830558247db454c37d3624a85ecf",
+    "url": "/domas/static/js/2.197989c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e7af5b5e78223605fd8",
+    "url": "/domas/static/js/main.db1d93f4.chunk.js"
+  },
+  {
+    "revision": "4c33f80dee4b1b5d0677",
+    "url": "/domas/static/js/runtime-main.3c5ca500.js"
+  },
+  {
+    "revision": "27b52bbb8b84034aa2117a8220cbf9f8",
+    "url": "/domas/static/media/pictures.27b52bbb.jpg"
+  }
+]);
